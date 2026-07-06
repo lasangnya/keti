cfab841e5b4b27c8efa579426f29cb6c12ec6f80
Lasan <lasan.gonsal@gmail.com>
2026-07-06 19:18:10 +0200

    feat : string constants

---
+++ lib/constants/app_strings.dart
@@ -0,0 +1,5 @@
+class AppStrings {
+  static const String dashboard = 'Dashboard';
+  static const String breakReminders = 'Break Reminders';
+  static const String hydrationReminders = 'Hydration Reminders';
+}
