9cde9a0ba2b3d090dcde97762c202e20971d73fa
Lasan <lasan.gonsal@gmail.com>
2026-07-28 15:43:40 +0200

    chore : integrate firebase core configuration for macos
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:keti/application/theme/theme_provider.dart';
 import 'package:keti/application/user_activity/user_activity_provider.dart';
+import 'package:keti/firebase_options.dart';
 import 'package:keti/presentation/pages/home/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: KetiApp()));
 }
 
