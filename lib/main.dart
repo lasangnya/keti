b21d02544dd8d2b6a92d01d396cb911a2c244ffb
Lasan <lasan.gonsal@gmail.com>
2026-07-29 18:16:15 +0200

    refactor : remove user activity key logger
    The study collects no keystroke or pointer data (GDPR minimization)
    
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:keti/application/theme/theme_provider.dart';
-import 'package:keti/application/user_activity/user_activity_provider.dart';
 import 'package:keti/core/constants/app_config.dart';
 import 'package:keti/core/services/firebase/auth_service.dart';
 import 'package:keti/firebase_options.dart';
@@ -26,52 +23,17 @@ void main() async {
   runApp(const ProviderScope(child: KetiApp()));
 }
 
-class KetiApp extends ConsumerStatefulWidget {
+class KetiApp extends ConsumerWidget {
   const KetiApp({super.key});
 
   @override
-  ConsumerState<KetiApp> createState() => _KetiAppState();
-}
-
-class _KetiAppState extends ConsumerState<KetiApp> {
-  @override
-  void initState() {
-    super.initState();
-    HardwareKeyboard.instance.addHandler(_handleKeyEvent);
-  }
-
-  @override
-  void dispose() {
-    HardwareKeyboard.instance.removeHandler(_handleKeyEvent);
-    super.dispose();
-  }
-
-  bool _handleKeyEvent(KeyEvent event) {
-    if (event is KeyDownEvent) {
-      ref.read(userActivityProvider.notifier).logKeyboardStroke(
-            event.logicalKey.debugName ?? 'Unknown Key',
-          );
-    }
-    return false; // Allow event to propagate
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final theme = ref.watch(appThemeProvider);
 
-    return Listener(
-      onPointerMove: (_) => ref.read(userActivityProvider.notifier).logMouseMovement(),
-      onPointerDown: (_) => ref.read(userActivityProvider.notifier).logMouseClick(),
-      onPointerSignal: (signal) {
-        if (signal is PointerScrollEvent) {
-          ref.read(userActivityProvider.notifier).logMouseScroll();
-        }
-      },
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: theme,
-        home: const KetiHomePage(),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: theme,
+      home: const KetiHomePage(),
     );
   }
 }
